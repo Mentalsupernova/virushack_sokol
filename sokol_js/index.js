fab7e66d9f966a50c7cfd875f89dc628da159852
mentalsupernova <mentalsupernovafox@gmail.com>
2020-05-04 13:49:07 +0300

    join cmd

--- sokol_js/index.js
+++ sokol_js/index.js
@@ -15,23 +15,24 @@ client.on("ready", () => {
     console.log("I am alive!");
 });
 
-client.on("message", async mesage => {
-    console.log('${message.author.username} said: ${message.content}');
-});
 
 client.on("message", async message => {
-    const prefix = "~";
+        const prefix = "~";
 
-        if (!message.author.bot) return;
-        if (!message.guild) return;
-        if (!messgae.content.startsWith(prefix)) return;
 
         const args = message.content.slice(prefix.length).trim().split(/ +/g);
         const cmd = args.shift().toLowerCase();
-
+        console.log(args);
     if (cmd === "ping"){
         const msg = await message.channel.send("123");
         msg.edit('Pong\n');
+    }
+    if (cmd === "join"){
+        if (message.member.voice.channel){
+            const conn = await message.member.voice.channel.join();
+        } else {
+           message.reply("errr"); 
+        }
     }
 });
 
