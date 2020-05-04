4edb7d45da6150982c9c075fdfece9d81123ce89
mentalsupernova <mentalsupernovafox@gmail.com>
2020-05-04 14:23:50 +0300

    Добавленна запись

--- sokol_js/index.js
+++ sokol_js/index.js
@@ -1,6 +1,6 @@
 const { Client } = require("discord.js");
 const { config } = require("dotenv"); 
-
+const fs = require('fs');
 const client = new Client({
     disableEveryone: true
 });
@@ -16,6 +16,12 @@ client.on("ready", () => {
 });
 
 
+function generateOutputFile(channel, member) {
+  // use IDs instead of username cause some people have stupid emojis in their name
+  const fileName = `./recordings/${channel.id}-${member.id}-${Date.now()}.pcm`;
+  return fs.createWriteStream(fileName);
+}
+
 client.on("message", async message => {
         const prefix = "~";
 
@@ -30,6 +36,8 @@ client.on("message", async message => {
     if (cmd === "join"){
         if (message.member.voice.channel){
             const conn = await message.member.voice.channel.join();
+            const audio = conn.receiver.createStream(message.member, {mode: 'pcm'});
+            audio.pipe(fs.createWriteStream('user_audio'));
         } else {
            message.reply("errr"); 
         }
